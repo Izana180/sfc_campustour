0a574fca3748d40e2b8b61d6684c40274025e095
Izana180 <valleymouth0402@yahoo.co.jp>
2025-01-04 22:14:18 +0900

    explore

--- app/explore/page.js
+++ app/explore/page.js
@@ -40,14 +40,11 @@ export default function Home() {
     const isCorrect = selectedOption[id] === correctAnswer;
     const isAnswered = selectedOption[id] !== undefined;
 
-
-    console.log('isAnswered:', isAnswered, 'isCorrect:', isCorrect, 'selectedOption:', selectedOption);
-
     return (
       <section className={styles.quizContainer} id={id}>
         <h2>{question}</h2>
         <div className={styles.quizImage}>
-          <Image width={720} height={476} src={imageSrc} alt="クイズ画像" />
+          <Image width={720} height={476} src={imageSrc} alt="クイズ画像" style={{ width: '80', height: 'auto' }} />
         </div>
         <div className={styles.quizOptions}>
           {options.map((option, index) => (
@@ -70,7 +67,7 @@ export default function Home() {
               <div className={styles.feedbackCorrect}>
                 <p>正解！</p>
                 <Image width={720} height={476} src={feedbackImage} alt="フィードバック画像" />
-                <p>{feedbackText}</p>
+                <p dangerouslySetInnerHTML={{__html: feedbackText,}}></p>
               </div>
             ) : (
               <div className={styles.feedbackIncorrect}>不正解！</div>
@@ -120,7 +117,7 @@ export default function Home() {
             className={styles.mapImage}
             width={1080}
             height={714}
-            src="https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/SFCMap.png"
+            src="/SFCMap.png"
             alt="地図"
           />
         </div>
@@ -129,71 +126,71 @@ export default function Home() {
       {renderQuizSection(
         'quiz1',
         'Q1: 隠れている部分に書かれている言葉は？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
+        '/Quiz01.jpg',
         ['SFC', 'FAB', 'MCR'],
         'FAB',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        'FABでは3Dプリンターやレーザーカッターが無料で利用でき、様々なモノ作りを体験することができます。合言葉：不動'
+        '/IMG_6151-scaled.jpg',
+        '<span style="color: yellow;">FAB</span>では3Dプリンターやレーザーカッターが無料で利用でき、様々なモノ作りを体験することができます。<br />合言葉：不動'
       )}
 
       {renderQuizSection(
         'quiz2',
         'Q2: SFCのキャンパス内には、容器を変更することで20円引きになる食事処があります。何というお店でしょう？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
+        '/Quiz01.jpg',
         ['Subway', '学生食堂', 'レディバード'],
         'レディバード',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        'レディバードでは、『リターナブル容器』に変更することでゴミ資源を削減しつつお得に食べることができます。合言葉：回り道'
+        '/IMG_6151-scaled.jpg',
+        'レディバードでは、<span style="color: yellow;">『</span>リターナブル容器<span style="color: yellow;">』</span>に変更することでゴミ資源を削減しつつお得に食べることができます。<br />合言葉：回り道'
       )}
 
       {renderQuizSection(
         'quiz3',
         'Q3:鴨池前の芝生でランチを食べるためにレジャーシートを無料で借りれる場所があるのですが、どこでしょう？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
+        '/Quiz01.jpg',
         ['メディアセンター', '食堂', '生協'],
         '食堂',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        '食堂で無料でレジャーシートが借りられます。気分転換に鴨池前の芝生でランチをしてみましょう！合言葉：サザンカ'
+        '/IMG_6151-scaled.jpg',
+        '食堂で無料でレジャーシートが借りられます。気分転換に鴨池前の芝生でランチをしてみましょう！<br />合言葉：サザンカ'
       )}
 
       {renderQuizSection(
         'quiz4',
         'Q4: 進級するために必要な単位を取れずに原級しても、4年で卒業できる制度がありますが、何というでしょう？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
+        '/Quiz01.jpg',
         ['フェニックス', '4年卒業制度', 'ゾンビ'],
         'フェニックス',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        'DSや体育、研究会などによって故意でなく原級してしまうSFC生が多々見受けられます。このような事にならないように履修はしっかり考えましょう'
+        '/quiz2.jpg',
+        'DSや体育、研究会などによって<span style="color: yellow;">故意</span>でなく原級してしまうSFC生が多々見受けられます。このような事にならないように履修はしっかり考えましょう'
       )}
 
       {renderQuizSection(
         'quiz5',
         'Q5: シグマ館には学生が自由にくつろげるスペースがあります。何という場所でしょう？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
+        '/Quiz01.jpg',
         ['鴨池ラウンジ', 'ほっとスペース', '憩いの広場'],
         '鴨池ラウンジ',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        'コンビニに併設している鴨池ラウンジには仕切りのついたソファや大人数用の机があります。隙間時間に行ってみましょう！'
+        '/IMG_6151-scaled.jpg',
+        'コンビニに併設している鴨池<span style="color: yellow;">ラウンジ</span>には仕切りのついたソファや大人数用の机があります。隙間時間に行ってみましょう！'
       )}
 
       {renderQuizSection(
         'quiz6',
         'Q6: ガンマ館にある施設はどれ？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
+        '/Quiz01.jpg',
         ['メディカルセンター', 'デザインスタジオ', 'ジム'],
         'ジム',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        'ガンマ館は体育館アリーナだけではなく、学生が無料で利用できるジムがあります！運動したい時に行ってみましょう！'
+        '/IMG_6151-scaled.jpg',
+        'ガンマ館は体育館<span style="color: yellow;">アリーナ</span>だけではなく、学生が無料で利用できるジムがあります！運動したい時に行ってみましょう！'
       )}
 
       {renderQuizSection(
         'quiz7',
         'Q7: SFCではビデオカメラや本格カメラをレンタルすることができます。どこで借りられるでしょう？',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/Quiz01.jpg',
-        ['電子機器サポートセンター', 'AVカウンター', 'カメラの窓口'],
+        '/Quiz01.jpg',
+        ['サポートセンター', 'AVカウンター', 'カメラの窓口'],
         'AVカウンター',
-        'https://web.sfc.keio.ac.jp/~t22934mw/wpx/wp-content/uploads/2024/12/IMG_6151-scaled.jpg',
-        'メディアセンター内のAVカウンターでは、SFC生であれば無料でカメラをレンタルできます！学生生活の思い出や経験を記録として残してみませんか？'
+        '/IMG_6151-scaled.jpg',
+        'メディアセンター内の<span style="color: yellow;">AVカウンター</span>では、SFC生であれば無料で<span style="color: yellow;">カメラ</span>をレンタルできます！学生生活の思い出や経験を記録として残してみませんか？'
       )}
     </>
   );
